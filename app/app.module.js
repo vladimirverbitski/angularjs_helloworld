0db4d22e38f94d5917cb29df1e6bf09a3334956e
vladimir <vova.maou@gmail.com>
2018-05-14 21:56:22 +0600

    commited from home

--- app/app.module.js
+++ app/app.module.js
@@ -1,12 +1,12 @@
 import angular from 'angular';
 import uiRouter from  'angular-ui-router';
 
-import AppComponent from './app.component';
-import ComponentsModule from './components/components.module';
-import CommonModule from './common/common.module';
+import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
+import { CommonModule } from './common/common.module';
 import './app.css';
 
-export const app = angular
+export const AppModule = angular
     .module('app', [
         ComponentsModule,
         CommonModule,
